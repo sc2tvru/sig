a664e00fbdc31854655ff79b250a7bdfe33af432
Smirnov Danil <aulust@gmail.com>
2012-09-02 20:23:45 +0400

    Update cleaner. shr

--- php/cleaner.php
+++ php/cleaner.php
@@ -3,28 +3,37 @@ require_once 'config.php';
 require_once 'utils.php';
 require_once 'db.php';
 
-// время жизни необновляемого бара
-define( 'SIG_TTL', 259200 );
-
 $db = new MySqlDb( DB_HOST, DB_NAME, DB_USER, DB_PASSWORD );
 
 $queryString = 'DELETE FROM '.DB_TABLE_PREFIX.'data WHERE ';
 
+$debugOutput = '';
+$doesOldSigExist = false;
+
 foreach ( glob( SIG_BASEDIR.'*.png' ) as $filename ) {
 	if ( time() - filemtime( $filename ) > SIG_TTL ) {
-		//echo "$filename - ".date( 'd M H:i:s', filemtime( $filename ) ). "\n";
+		$doesOldSigExist = true;
+		// echo "$filename - ".date( 'd M H:i:s', filemtime( $filename ) ). "\n";
 		preg_match( "#/([\d]+).png#", $filename, $match );
 		
 		if ( $match[ 1 ] !== '' ) {
 			$queryString .= 'sigId="'.$match[ 1 ].'" OR ';
+			$debugOutput .= $match[ 1 ] . ', ';
 		}
 		@unlink( $filename );
 	}
 }
 
-$queryString = substr( $queryString, 0, strlen( $queryString ) - 4 );
-// echo $queryString;
-echo date( 'd M H:i:s', time() );
+if ( $doesOldSigExist ) {
+	$queryString = substr( $queryString, 0, strlen( $queryString ) - 4 );
+	$queryResult = $db->Query( $queryString );
 
-$queryResult = $db->Query( $queryString );
+	if ( $queryResult ) {
+		$debugOutput = 'Cleaner delete sigs:' . $debugOutput;
+		SaveForDebug( $debugOutput );
+	}
+}
+else {
+	SaveForDebug( 'Cleaner doesn\'t found old sigs.' );
+}
 ?>
