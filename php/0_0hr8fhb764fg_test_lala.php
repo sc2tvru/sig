9069250ca909ace58eda2775d7b7331ffdedca17
shr <forshr@gmail.com>
2013-03-29 01:40:22 +0400

    Update portraits. Remove useless htaccess.

--- php/0_0hr8fhb764fg_test_lala.php
+++ php/0_0hr8fhb764fg_test_lala.php
@@ -10,7 +10,9 @@ require_once 'sig.php';
 // http://eu.battle.net/sc2/ru/profile/215861/2/Aaron/
 // http://eu.battle.net/sc2/ru/profile/74373/2/BVOne/
 // http://eu.battle.net/sc2/ru/profile/2175278/1/BRuǂMakka/
-$isSigCreated = Sig::Create( '1', 2175278, 1, 'BRuǂMakka', 11, 0, 'ru', 'RU', 123 );
+// http://eu.battle.net/sc2/en/profile/3391476/1/IlIlIlIlIlIl/
+// http://sc2ranks.com
+$isSigCreated = Sig::Create( '1', 3391476, 1, 'IlIlIlIlIlIl', 11, 0, 'ru', 'RU', 123 );
 
 // http://us.battle.net/sc2/en/profile/1644048/1/IAmTheWalrus/
 //$isSigCreated = $sig->Create( '1', 1644048, 1, 'IAmTheWalrus', 130, 0, 'ru', 'US', 777 );
